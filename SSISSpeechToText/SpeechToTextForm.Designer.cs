9b95838bfce1a471ed2e21078ad05476557c7941
Keith Martin <keith.martin@outlook.com.au>
2018-02-16 10:56:58 +1000

    Name fields on form, add tool tips, add license file

--- SSISSpeechToText/SpeechToTextForm.Designer.cs
+++ SSISSpeechToText/SpeechToTextForm.Designer.cs
@@ -33,23 +33,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TBSubscriptionKey = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.CBChannelSeparation = new System.Windows.Forms.ComboBox();
+            this.CBLanguage = new System.Windows.Forms.ComboBox();
+            this.CBOperationMode = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CBFileNames = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.TBChannel = new System.Windows.Forms.TextBox();
+            this.RTBAbout = new System.Windows.Forms.RichTextBox();
             this.BTNCancel = new System.Windows.Forms.Button();
             this.BTNOk = new System.Windows.Forms.Button();
             this.ToolTipSpeechToText = new System.Windows.Forms.ToolTip(this.components);
@@ -85,23 +85,22 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Language";
             // 
-            // textBox1
+            // TBSubscriptionKey
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(178, 20);
-            this.textBox1.TabIndex = 2;
-            this.ToolTipSpeechToText.SetToolTip(this.textBox1, "Enter your subscription key for \r\nAzure Cognitive Services\r\nBing Speech API");
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TBSubscriptionKey.Location = new System.Drawing.Point(112, 19);
+            this.TBSubscriptionKey.Name = "TBSubscriptionKey";
+            this.TBSubscriptionKey.Size = new System.Drawing.Size(178, 20);
+            this.TBSubscriptionKey.TabIndex = 2;
+            this.ToolTipSpeechToText.SetToolTip(this.TBSubscriptionKey, "Enter your subscription key for \r\nAzure Cognitive Services\r\nBing Speech API");
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.comboBox3);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.CBChannelSeparation);
+            this.groupBox1.Controls.Add(this.CBLanguage);
+            this.groupBox1.Controls.Add(this.CBOperationMode);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.TBSubscriptionKey);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
@@ -110,30 +109,6 @@
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Speech Recognition Settings";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(112, 46);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(178, 21);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(112, 74);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(178, 21);
-            this.comboBox2.TabIndex = 5;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(112, 102);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(178, 21);
-            this.comboBox3.TabIndex = 6;
             // 
             // label4
             // 
@@ -144,9 +119,51 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Channel Separation";
             // 
+            // CBChannelSeparation
+            // 
+            this.CBChannelSeparation.FormattingEnabled = true;
+            this.CBChannelSeparation.Items.AddRange(new object[] {
+            "Mono/Ignore",
+            "Separate Left and Right"});
+            this.CBChannelSeparation.Location = new System.Drawing.Point(112, 102);
+            this.CBChannelSeparation.Name = "CBChannelSeparation";
+            this.CBChannelSeparation.Size = new System.Drawing.Size(178, 21);
+            this.CBChannelSeparation.TabIndex = 6;
+            this.ToolTipSpeechToText.SetToolTip(this.CBChannelSeparation, "If you have stereo input, the decide if you want each channel processed separatel" +
+        "y");
+            // 
+            // CBLanguage
+            // 
+            this.CBLanguage.FormattingEnabled = true;
+            this.CBLanguage.Items.AddRange(new object[] {
+            "American English",
+            "British English",
+            "German",
+            "Spanish",
+            "French",
+            "Italian",
+            "Mandarin"});
+            this.CBLanguage.Location = new System.Drawing.Point(112, 74);
+            this.CBLanguage.Name = "CBLanguage";
+            this.CBLanguage.Size = new System.Drawing.Size(178, 21);
+            this.CBLanguage.TabIndex = 5;
+            this.ToolTipSpeechToText.SetToolTip(this.CBLanguage, "Select the language that is used in the speech");
+            // 
+            // CBOperationMode
+            // 
+            this.CBOperationMode.FormattingEnabled = true;
+            this.CBOperationMode.Items.AddRange(new object[] {
+            "Short Dictation",
+            "Long Dictation"});
+            this.CBOperationMode.Location = new System.Drawing.Point(112, 46);
+            this.CBOperationMode.Name = "CBOperationMode";
+            this.CBOperationMode.Size = new System.Drawing.Size(178, 21);
+            this.CBOperationMode.TabIndex = 4;
+            this.ToolTipSpeechToText.SetToolTip(this.CBOperationMode, "Select the dictation mode that you wish to use.");
+            // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.comboBox4);
+            this.groupBox2.Controls.Add(this.CBFileNames);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Location = new System.Drawing.Point(318, 12);
             this.groupBox2.Name = "groupBox2";
@@ -154,6 +171,15 @@
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Input Settings";
+            // 
+            // CBFileNames
+            // 
+            this.CBFileNames.FormattingEnabled = true;
+            this.CBFileNames.Location = new System.Drawing.Point(73, 13);
+            this.CBFileNames.Name = "CBFileNames";
+            this.CBFileNames.Size = new System.Drawing.Size(178, 21);
+            this.CBFileNames.TabIndex = 1;
+            this.ToolTipSpeechToText.SetToolTip(this.CBFileNames, "Select the column that has the speech recording file names to process");
             // 
             // label5
             // 
@@ -164,14 +190,6 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "File Names";
             // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(73, 13);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(178, 21);
-            this.comboBox4.TabIndex = 1;
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label8);
@@ -179,7 +197,7 @@
             this.groupBox3.Controls.Add(this.label6);
             this.groupBox3.Controls.Add(this.textBox4);
             this.groupBox3.Controls.Add(this.textBox3);
-            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.TBChannel);
             this.groupBox3.Location = new System.Drawing.Point(584, 13);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(262, 163);
@@ -187,35 +205,14 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Output Column Names";
             // 
-            // textBox2
+            // label8
             // 
-            this.textBox2.Location = new System.Drawing.Point(78, 19);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(178, 20);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(78, 45);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(178, 20);
-            this.textBox3.TabIndex = 4;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(78, 71);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(178, 20);
-            this.textBox4.TabIndex = 5;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 22);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 13);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Channel";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Timecodes";
             // 
             // label7
             // 
@@ -226,22 +223,48 @@
             this.label7.TabIndex = 8;
             this.label7.Text = "Speech";
             // 
-            // label8
+            // label6
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 74);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(30, 13);
-            this.label8.TabIndex = 9;
-            this.label8.Text = "Time";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Channel";
             // 
-            // richTextBox1
+            // textBox4
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(13, 183);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(751, 141);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = "";
+            this.textBox4.Location = new System.Drawing.Point(78, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(178, 20);
+            this.textBox4.TabIndex = 5;
+            this.ToolTipSpeechToText.SetToolTip(this.textBox4, "Enter the column name for the time codes.\r\nUsed when channel separation is active" +
+        ".");
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(78, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 20);
+            this.textBox3.TabIndex = 4;
+            this.ToolTipSpeechToText.SetToolTip(this.textBox3, "Enter the name of the column that will have all the text returned");
+            // 
+            // TBChannel
+            // 
+            this.TBChannel.Location = new System.Drawing.Point(78, 19);
+            this.TBChannel.Name = "TBChannel";
+            this.TBChannel.Size = new System.Drawing.Size(178, 20);
+            this.TBChannel.TabIndex = 3;
+            this.ToolTipSpeechToText.SetToolTip(this.TBChannel, "Enter the name of the output column that has the channel name in it");
+            // 
+            // RTBAbout
+            // 
+            this.RTBAbout.Enabled = false;
+            this.RTBAbout.Location = new System.Drawing.Point(13, 183);
+            this.RTBAbout.Name = "RTBAbout";
+            this.RTBAbout.Size = new System.Drawing.Size(751, 141);
+            this.RTBAbout.TabIndex = 8;
+            this.RTBAbout.Text = resources.GetString("RTBAbout.Text");
             // 
             // BTNCancel
             // 
@@ -271,7 +294,7 @@
             this.ClientSize = new System.Drawing.Size(858, 333);
             this.Controls.Add(this.BTNOk);
             this.Controls.Add(this.BTNCancel);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.RTBAbout);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -281,6 +304,7 @@
             this.Name = "SpeechToTextForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Speech To Text";
+            this.Load += new System.EventHandler(this.SpeechToTextForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -295,15 +319,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TBSubscriptionKey;
         private System.Windows.Forms.ToolTip ToolTipSpeechToText;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox CBChannelSeparation;
+        private System.Windows.Forms.ComboBox CBLanguage;
+        private System.Windows.Forms.ComboBox CBOperationMode;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox CBFileNames;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label8;
@@ -311,8 +335,8 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox TBChannel;
+        private System.Windows.Forms.RichTextBox RTBAbout;
         private System.Windows.Forms.Button BTNCancel;
         private System.Windows.Forms.Button BTNOk;
     }
--- SSISSpeechToText/SpeechToTextForm.cs
+++ SSISSpeechToText/SpeechToTextForm.cs
@@ -16,9 +16,18 @@ namespace Martin.SQLServer.Dts
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
+        private void SpeechToTextForm_Load(object sender, EventArgs e)
+        {
+            RTBAbout.SelectAll();
+            RTBAbout.SelectedRtf = @"{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang3081\deflangfe3081{\fonttbl{\f0\fswiss\fprq2\fcharset0 Calibri;}}
+{\*\generator Riched20 10.0.16299}
+            {\*\mmathPr\mnaryLim0\mdispDef1\mwrapIndent1440 }\viewkind4\uc1
+\pard\widctlpar\sa160\sl252\slmult1\b\f0\fs24 Speech to Text\par
+\b0\fs22\'a9 Keith Martin 2018\par
+Provides the ability to read recorded sound files from the file system, and send them to Microsoft Cognitive Services, Bing Speech API.\par
+If you have stereo recordings, with one channel per person, then these can be processed separately, with timecodes added to indicate the start time of each persons speech.\par
+}";
         }
     }
 }
